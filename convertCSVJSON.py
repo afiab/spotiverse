c553bc511c0d30bb4af2d3e0ed7aa5b632add04b
afiab <abidica30@stuy.edu>
2023-11-05 01:40:29 -0400

    fixed python pandas script to format json file in a more accessible manner

--- convertCSVJSON.py
+++ convertCSVJSON.py
@@ -1,3 +1,19 @@
 import pandas as pd
+import json
 df = pd.read_csv (r'C:\Users\afiab\SoftDev\spotiverse\universal_top_spotify_songs.csv')
-df.to_json (r'C:\Users\afiab\SoftDev\spotiverse\universal_top_spotify_songs.json')
+# clist = "{"
+songs = {}
+for idx, row in df.iterrows():
+    songs[row["name"]] = {
+        "country":row["country"],
+        "artists":row["artists"],
+        "popularity":row["popularity"]
+    }
+    # clist+=("{"+row["name"]+":["+row["country"]+","+row["artists"]+","+str(row["popularity"])+"]}")
+# clist+="}"
+# print(clist);
+# df.to_json (r'C:\Users\afiab\SoftDev\spotiverse\universal_top_spotify_songs.json', indent=4)
+# json_output = json.dumps(songs, indent=4)
+
+with open(r"C:\Users\afiab\SoftDev\spotiverse\universal_top_spotify_songs.json","w") as f:
+    json.dump(songs,f, indent=4)
