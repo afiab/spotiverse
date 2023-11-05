72d198214db282dd9a44c90ce8efe1f1daa6949d
afiab <abidica30@stuy.edu>
2023-11-05 03:49:49 -0500

    the bar for the game now shows on the site

--- play.js
+++ play.js
@@ -1,5 +1,7 @@
+var screenadjust = 200;
 let blocks = []
 let level = 0;
+let score = 0;
 let platform;
 let block;
 let wide = 300; //score*4 
@@ -13367,26 +13369,30 @@ let songs = {
 }
 
 function setup() {
-  var cnv = createCanvas(500, 300);
-  cnv.parent("sketchHolder");
+//   var cnv = 
+  createCanvas(500, 300);
+//   cnv.parent("sketchHolder");
   world.gravity = 2;
   background(250, 240, 230);
   input = createInput("Type a Top50 Song from xyzabc");
-  input.position(16, 2);
+  input.position(16, 2+screenadjust);
   input.size(400);
-  input.parent("sketchHolder");
-  stroke(0);
-  text("Level: "+level, 15, 55);
+//   input.parent("sketchHolder");
 
-  platform = new Sprite(width/2, height/2, 100,5,'k');
+  platform = new Sprite(width/2, height-10, 400,5,'k');
   platform.color = "blue";
 
   button = createButton('submit');
-  button.position(input.x + input.width, 25);
-  // button.mousePressed(greet);
+  button.position(input.x + input.width, 2+screenadjust+1);
+//   button.parent("sketchHolder");
 }
 
 function draw() {
-  const country = input.value();
-
+    clear();
+    stroke(0);
+    text("Level: "+level, 15, 55);
+    if(mouse.presses()){
+        block = new Sprite(mouseX, 0-score*size,size,size,'d');
+        block.push(block);
+    }
 }
