f66c7dd9715861dcf8b996e0471df1d917c6a215
afiab <abidica30@stuy.edu>
2023-11-05 04:07:50 -0500

    restarting

--- play.js
+++ play.js
@@ -1,10 +1,3 @@
-var screenadjust = 200;
-let blocks = []
-let level = 0;
-let score = 0;
-let platform;
-let block;
-let wide = 300; //score*4 
 let songs = {
     "iPlan": {
         "country": "ZA",
@@ -13368,31 +13361,3 @@ let songs = {
     }
 }
 
-function setup() {
-//   var cnv = 
-  createCanvas(500, 300);
-//   cnv.parent("sketchHolder");
-  world.gravity = 2;
-  background(250, 240, 230);
-  input = createInput("Type a Top50 Song from xyzabc");
-  input.position(16, 2+screenadjust);
-  input.size(400);
-//   input.parent("sketchHolder");
-
-  platform = new Sprite(width/2, height-10, 400,5,'k');
-  platform.color = "blue";
-
-  button = createButton('submit');
-  button.position(input.x + input.width, 2+screenadjust+1);
-//   button.parent("sketchHolder");
-}
-
-function draw() {
-    clear();
-    stroke(0);
-    text("Level: "+level, 15, 55);
-    if(mouse.presses()){
-        block = new Sprite(mouseX, 0-score*size,size,size,'d');
-        block.push(block);
-    }
-}
